341731a0624f5417e7ac98c1414151879d58f0d3
Jean Pablo Vieira de Mello <jeanpvmello@gmail.com>
2025-03-11 18:57:56 -0300

    Update autoinit.bat

--- autoinit.bat
+++ autoinit.bat
@@ -1,2 +1,2 @@
 cd "C:\caminho\do\repo\telegram-automessager"
-"C:\caminho\do\meu\ambiente\python\Scripts\python.exe" .\telegram_notif.py .\message_files\otimismo.txt
+"C:\caminho\do\meu\ambiente\python\Scripts\python.exe" .\telegram_notif.py .\message_files\otimismo.txt --seg-min 5 --seg-max 3600
